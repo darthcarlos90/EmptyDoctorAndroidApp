d993a0b998afbf477cfd64ebcceea5b3280c2a8f
Carlos Tirado <carlos@cratebind.com>
2018-07-31 16:18:25 +1000

    Finalized the Main Activity, need to work on setting up the navigation to the other fragments.

--- app/src/main/java/yourteamnumber/com/seshealthdoctor/Activities/MainActivity.java
+++ app/src/main/java/yourteamnumber/com/seshealthdoctor/Activities/MainActivity.java
@@ -1,10 +1,17 @@
 package yourteamnumber.com.seshealthdoctor.Activities;
 
+import android.app.Fragment;
 import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
 
 import yourteamnumber.com.seshealthdoctor.R;
 
@@ -63,16 +70,111 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // the default fragment on display is the patient information
-//        currentState = MenuStates.HOME_PAGE;
-//
-//        // go look for the main drawer layout
-//        mDrawerLayout = findViewById(R.id.main_drawer_layout);
-//
-//        toolbar = findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
+        currentState = MenuStates.HOME_PAGE;
+
+        // go look for the main drawer layout
+        mDrawerLayout = findViewById(R.id.main_drawer_layout);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        // Set up the menu button
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
+
+
+        // Setup the navigation drawer, most of this code was taken from:
+        // https://developer.android.com/training/implementing-navigation/nav-drawer
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        // set item as selected to persist highlight
+                        menuItem.setChecked(true);
+                        // close drawer when item is tapped
+                        mDrawerLayout.closeDrawers();
+
+                        // Using a switch to see which item on the menu was clicked
+                        switch (menuItem.getItemId()) {
+                            // You can find these id's at: res -> menu -> drawer_view.xml
+
+                        }
+
+                        return true;
+                    }
+                });
+
+
+        // If you need to listen to specific events from the drawer layout.
+        mDrawerLayout.addDrawerListener(
+                new DrawerLayout.DrawerListener() {
+                    @Override
+                    public void onDrawerSlide(View drawerView, float slideOffset) {
+                        // Respond when the drawer's position changes
+                    }
+
+                    @Override
+                    public void onDrawerOpened(View drawerView) {
+                        // Respond when the drawer is opened
+                    }
+
+                    @Override
+                    public void onDrawerClosed(View drawerView) {
+                        // Respond when the drawer is closed
+                    }
+
+                    @Override
+                    public void onDrawerStateChanged(int newState) {
+                        // Respond when the drawer motion state changes
+                    }
+                }
+        );
+
+
+        // More on this code, check the tutorial at http://www.vogella.com/tutorials/AndroidFragments/article.html
+        fragmentManager = getFragmentManager();
+
+        // Add the default Fragment once the user logged in
+        FragmentTransaction ft = fragmentManager.beginTransaction();
+        // TODO: Add the fragment that is needed here as an introduction
+        //ft.add(R.id.fragment_container, new PatientInformationFragment());
+        ft.commit();
+
     }
 
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
 
+    /**
+     * This function changes the title of the fragment.
+     *
+     * @param newTitle The new title to write in the
+     */
+    public void ChangeTitle(String newTitle) {
+        toolbar.setTitle(newTitle);
+    }
+
+
+    /**
+     * This function allows to change the content of the Fragment holder
+     * @param fragment The fragment to be displayed
+     */
+    private void ChangeFragment(Fragment fragment) {
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.fragment_container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
 }
